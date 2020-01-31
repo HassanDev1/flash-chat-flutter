486b2a1aa72cf651606bbe91c5b56619baabe8b8
Hassan78 <hassan.ali@g.austincc.edu>
2020-01-30 22:40:18 -0600

    added firestore firebase as a backend to register

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -8,6 +10,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String password;
+  final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +36,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration: InputDecoration(
                 hintText: 'Enter your email',
@@ -55,6 +61,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration: InputDecoration(
                 hintText: 'Enter your password',
@@ -78,8 +85,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             RoundedButton(
               title: 'Register',
-             onPressed: () {
+             onPressed: () async{
                     //implement registration functionality;
+                    try{
+                    final newUser = await _auth.createUserWithEmailAndPassword(email: email,password: password);
+                    if(newUser != null){
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    }
+                    catch(e){
+                      print(e);
+                    }
                   },
           colour: Colors.blueAccent,
             ),
